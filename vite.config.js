54c1b12d374beb294a0f568eb18bac77bb5e3a18
alexjamesmx <sg.alex32@gmail.com>
2024-04-10 09:02:56 -0600

    git pages

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,20 @@ import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 dotenv.config();
-export default defineConfig({
-  plugins: [react(), compression()],
-  define: {
-    "process.env": process.env,
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    base: "/",
+    plugins: [react(), compression()],
+
+    define: {
+      "process.env": process.env,
+    },
+    build: {
+      reportCompressedSize: true,
+    },
+  };
+  if (command !== "serve") {
+    config.base = "/pwa2evdev";
+  }
+  return config;
 });
