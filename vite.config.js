def88f53721adc3c549322995baacdf381e3190a
alexjamesmx <sg.alex32@gmail.com>
2024-04-10 09:09:54 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { compression } from "vite-plugin-compression2";
 dotenv.config();
 export default defineConfig(({ command }) => {
   const config = {
-    base: "/",
+    base: "/pwa2evdev",
     plugins: [react(), compression()],
 
     define: {
