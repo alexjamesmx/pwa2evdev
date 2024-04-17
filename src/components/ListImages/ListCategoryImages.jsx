90cbeab6b7816319c9227017c8a0edad1c7c168e
alexjamesmx <sg.alex32@gmail.com>
2024-04-16 21:17:38 -0600

    version final

--- src/components/ListImages/ListCategoryImages.jsx
+++ src/components/ListImages/ListCategoryImages.jsx
@@ -36,7 +36,7 @@ const ListCategoryImages = ({ images }) => {
 
   const renderImages = () =>
     images.length > 0 ? (
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 place-items-center ">
         {images.map((image, index) => (
           <Card
             key={index}
